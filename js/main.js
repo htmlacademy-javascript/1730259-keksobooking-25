6d21f06d33660a86a01be7a243d14c149d706e0c
Vladimir <mesvladimir@yandex.ru>
2022-02-20 22:13:08 +0300

    Добавление второй функции

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-const getRandomNumber = (numberFrom, numberNext, numberFloat = 0) => {
-  if (numberFrom >= 0 && numberNext >= 0 && numberFrom < numberNext && numberFloat >= 0) {
-    const rand = (Math.random() * (numberNext - numberFrom) + numberFrom).toFixed(numberFloat);
-    return Number(rand);
+const getRandomNumberSimple = (numberFrom, numberNext) => {
+  if (numberFrom >= 0 && numberNext >= 0 && numberFrom < numberNext) {
+    const randomSimple = Math.random() * (numberNext - numberFrom) + numberFrom;
+    return Number(Math.trunc(randomSimple));
   }
-  return new TypeError('диапазон указан некорректно');
+  return new TypeError('Некоректно указанны данные');
 };
-getRandomNumber(6, 5, 3);
+
+const getRandomNumberFloat = (numberFrom, numberNext, numberFloat = 0) => {
+  if (numberFrom >= 0 && numberNext >= 0 && numberFrom < numberNext && numberFloat >= 0) {
+    const randomFloat = (Math.random() * (numberNext - numberFrom) + numberFrom).toFixed(numberFloat);
+    return Number(randomFloat);
+  }
+  return new TypeError('Некоректно указанны данные');
+};
+
+getRandomNumberSimple(2.1, 9.5);
+getRandomNumberFloat(6, 5, 3);
