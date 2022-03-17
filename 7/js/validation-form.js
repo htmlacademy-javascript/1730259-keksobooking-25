e93b2bf7c836e0361fbadb5f58802eda53ca7e5b
Keks <keks@htmlacademy.ru>
2022-03-18 01:43:01 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/validation-form.js
+++ 7/js/validation-form.js
@@ -1,5 +1,25 @@
 import {adForm} from './change-page-form.js';
 
+const ROOMS_AND_GUESTS = {
+  '1': ['1'],
+  '2': ['2', '1'],
+  '3': ['3', '2', '1'],
+  '100': ['0'],
+};
+
+const TYPES_HOUSES_AND_PRICES = {
+  'Бунгало' : 0,
+  'Квартира': 1000,
+  'Отель' : 3000,
+  'Дом' : 5000,
+  'Дворец' : 10000,
+};
+
+const numberRoom = adForm.querySelector('[name="rooms"]');
+const numberSeats = adForm.querySelector('[name="capacity"]');
+const typesHousing = adForm.querySelector('[name="type"]');
+const pricesHousing = adForm.querySelector('[name="price"]');
+
 const pristine = new Pristine (adForm, {
   classTo: 'ad-form__element',
   errorClass: 'ad-form--invalid',
@@ -9,20 +29,16 @@ const pristine = new Pristine (adForm, {
   errorTextClass: 'ad-form__text-error'
 }, true);
 
-const numberRoom = adForm.querySelector('[name="rooms"]');
-const numberSeats = adForm.querySelector('[name="capacity"]');
-
-const ROOMS_AND_QUESTS = {
-  '1': ['1'],
-  '2': ['2', '1'],
-  '3': ['3', '2', '1'],
-  '100': ['0'],
+const changeColorError = () => {
+  const textElement = adForm.querySelectorAll('.ad-form__text-error');
+  textElement.forEach((element) => {element.style.color = '#ff0000';});
 };
 
-const compareRooms = () => ROOMS_AND_QUESTS[numberRoom.value].includes(numberSeats.value);
+const compareRooms = () => ROOMS_AND_GUESTS[numberRoom.value].includes(numberSeats.value);
 
-const ChangeNumberSeats = () => {
+const onChangeNumberSeats = () => {
   pristine.validate(numberSeats);
+  changeColorError();
 };
 
 const getRoomsErrorMessages = () => {
@@ -39,11 +55,16 @@ const getRoomsErrorMessages = () => {
 };
 
 pristine.addValidator(numberRoom, compareRooms, getRoomsErrorMessages);
-pristine.addValidator(numberSeats, compareRooms, getRoomsErrorMessages);
-numberRoom.addEventListener('change', ChangeNumberSeats);
+numberRoom.addEventListener('change', onChangeNumberSeats);
+
+typesHousing.addEventListener('change', () => {
+  pricesHousing.placeholder = TYPES_HOUSES_AND_PRICES[typesHousing.value];
+  typesHousing.min = TYPES_HOUSES_AND_PRICES[pricesHousing.value];
+});
 
 adForm.addEventListener('submit', (evt) => {
   if (!pristine.validate()) {
     evt.preventDefault();
+    changeColorError();
   }
 });
