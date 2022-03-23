15f7738c4cdeb3de5ce760e0c64a5e036c992105
Vladimir <mesvladimir@yandex.ru>
2022-03-23 22:04:29 +0300

    добовление файла для карты

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
 import {generateNewUsers, SIMILAR_AD_COUNT} from './data.js';
-import {renderCard} from './card.js';
+// import {renderCard} from './card.js';
 import {deactivatePage, activatePage} from './change-page-form.js';
 import './validation-form.js';
 import './slider.js';
+import './map.js';
 
 const generationData = generateNewUsers(SIMILAR_AD_COUNT);
 
-renderCard(generationData[0]);
+// renderCard(generationData[0]);
 
 deactivatePage();
 activatePage();
