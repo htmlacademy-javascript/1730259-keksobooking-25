bf6868561cf9275f502dec646f4cbefefdd703df
Keks <keks@htmlacademy.ru>
2022-03-26 13:39:20 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/data.js
+++ 9/js/data.js
@@ -124,4 +124,4 @@ const generateNewItem = () => {
 
 const generateNewUsers = (element) => Array.from({length: element}, generateNewItem);
 
-export {generateNewUsers,generateNewItem, SIMILAR_AD_COUNT};
+export {generateNewUsers,generateNewItem, SIMILAR_AD_COUNT, TIMES};
--- 9/js/reset-form.js
+++ 9/js/reset-form.js
@@ -1,7 +1,8 @@
 import {map, mainPinMarker, getAddress, CENTER_CITY_TOKYO, ZOOM_MAP} from './map.js';
 import {adForm} from './change-page-form.js';
+import {TIMES} from './data.js';
 import {typesHousing, MIN_PRICE_HOUSING, numberRoom, pricesHousing, numberSeats, timeIn, timeOut} from './validation-form.js';
-import {sliderElement} from './slider.js';
+import {sliderElement, RANGE_MIN, RANGE_MAX} from './slider.js';
 
 const resetButton = adForm.querySelector('.ad-form__reset');
 const titleForm = adForm.querySelector('#title');
@@ -16,17 +17,17 @@ const resetMainPin = (marker) => {
 const resetPrice = () => {
   sliderElement.noUiSlider.updateOptions({
     range: {
-      min: MIN_PRICE_HOUSING['flat'],
-      max: 100000,
+      min: RANGE_MIN,
+      max: RANGE_MAX,
     },
-    start: MIN_PRICE_HOUSING['flat'],
+    start: pricesHousing.placeholder,
   });
   pricesHousing.value = MIN_PRICE_HOUSING['flat'];
 };
 
 const resetTime = () => {
-  timeIn.value = '12:00';
-  timeOut.value = '12:00';
+  timeIn.value = TIMES[0];
+  timeOut.value = TIMES[0];
 };
 
 const resetForm = () => {
