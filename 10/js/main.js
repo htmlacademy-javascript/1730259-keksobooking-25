693939eec4eb20634c1927e67b263b8d87e447ed
Keks <keks@htmlacademy.ru>
2022-03-30 00:05:38 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,11 @@
+import {setUserFormSubmit} from './validation-form.js';
+import './slider.js';
+import {createData} from './map.js';
+import {SIMILAR_AD_COUNT} from './data.js';
+import {getResetForm} from './reset-form.js';
+import {getData} from './network.js';
+import './send-form.js';
+
+getData((dataUsers) => createData(dataUsers.slice(0, SIMILAR_AD_COUNT)));
+
+setUserFormSubmit(getResetForm);
---
+++ 10/js/reset-form.js
@@ -0,0 +1,59 @@
+import {map, mainPinMarker, getAddress, CENTER_CITY_TOKYO, ZOOM_MAP} from './map.js';
+import {adForm} from './change-page-form.js';
+import {typesHousing, MIN_PRICE_HOUSING, numberRoom, pricesHousing, numberSeats, timeIn, timeOut} from './validation-form.js';
+import {sliderElement, RANGE_MIN, RANGE_MAX} from './slider.js';
+
+const resetButton = adForm.querySelector('.ad-form__reset');
+const titleForm = adForm.querySelector('#title');
+const description = adForm.querySelector('#description');
+const featuresCheckbox = adForm.querySelectorAll('.features__checkbox');
+
+const selectTime = timeIn.options;
+const sv = selectTime.selectedIndex;
+
+const resetMainPin = (marker) => {
+  marker.setLatLng(CENTER_CITY_TOKYO);
+  map.setView(CENTER_CITY_TOKYO, ZOOM_MAP);
+};
+
+const resetPrice = () => {
+  sliderElement.noUiSlider.updateOptions({
+    range: {
+      min: RANGE_MIN,
+      max: RANGE_MAX,
+    },
+    start: pricesHousing.placeholder,
+  });
+  pricesHousing.value = MIN_PRICE_HOUSING['flat'];
+};
+
+const resetTime = () => {
+  timeIn.value = selectTime[sv].value;
+  timeOut.value = selectTime[sv].value;
+};
+
+const resetForm = () => {
+  resetPrice();
+  resetTime();
+  getAddress(CENTER_CITY_TOKYO);
+  titleForm.value = '';
+  description.value = '';
+  typesHousing.value = 'flat';
+  numberRoom.value = 1;
+  numberSeats.value = 3;
+  featuresCheckbox.forEach((value) => {
+    value.checked = false;
+  });
+};
+
+const getResetForm = () => {
+  resetMainPin(mainPinMarker);
+  resetForm();
+};
+
+resetButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  getResetForm();
+});
+
+export {getResetForm};
