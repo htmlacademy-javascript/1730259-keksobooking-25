dd40d1e560b6d021094384d35e626f1a9d93f415
Meshcheryakov Vladimir <89020510+Kvazim@users.noreply.github.com>
2022-03-30 09:55:39 +0300

    fix lint

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ import {setUserFormSubmit} from './validation-form.js';
 import './slider.js';
 import {createData} from './map.js';
 import {SIMILAR_AD_COUNT} from './data.js';
-import {getResetForm} from './reset-form.js';
 import {getData} from './network.js';
 import './send-form.js';
+import {showPopupMessage} from './util.js';
 
 getData((dataUsers) => createData(dataUsers.slice(0, SIMILAR_AD_COUNT)));
 
